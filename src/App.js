d88eb2d745d4a9253791b94cef70647a7d1a865d
dfjerry <53176182+dfjerry@users.noreply.github.com>
2020-05-14 18:54:40 +0700

    create react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <li>Go to market</li>
+          <li>Buy food</li>
+          <li>Make dinner</li>
+      </header>
+    </div>
+  );
+}
+
+export default App;
